9bc11ceaa2ad59ec74d7ac0a8ee2b206fd87d02e
BrouillardLiam <liambrouillard@outlook.fr>
2024-11-16 11:45:18 -0500

    FAIT: question5 ajout du case SELECT de gestion

--- src/main/java/com/atoudeft/serveur/GestionnaireEvenementServeur.java
+++ src/main/java/com/atoudeft/serveur/GestionnaireEvenementServeur.java
@@ -111,6 +111,58 @@ public class GestionnaireEvenementServeur implements GestionnaireEvenement {
                             cnx.envoyer("NOUVEAU NO "+t[0]+" existe");
                     }
                     break;
+                case "SELECT":
+                    
+                    if(cnx.getNumeroCompteClient() == null){
+                        cnx.envoyer("SELECT NO");
+                    }else{
+                        string numeroCompte;
+                        string typeCompte = evenement.getArgument().toLowerCase();
+                            
+                        if(typeCompte.equals("cheque")){
+                            numeroCompte = banque.getNumeroCompteParDefaut(cnx.getNumeroCompteClient());
+
+                            //s'assure qu'il y a un com^te chèque lié
+                            if (numeroCompte != null) {
+                                cnx.setNumeroCompteSelectionne(numeroCompte);
+                                cnx.envoyer("SELECT OK");
+                            } else {
+                                cnx.envoyer("SELECT NO");
+                            }
+                        }else if(typeCompte.equals("epargne")){
+
+                            //cherche le compte client dans la banque
+                            CompteClient compteClient = null;
+                            for(CompteClient compte : banque.getCompteClient()){
+                                if(compte.getNumeroCompteClient() == compteClient.getNumeroCompteClient()){
+                                    numeroCompte = compteClient.getNumeroCompteEpargne();
+                                    break;
+                                }
+                            }
+
+                            if(compteClient != null){
+                                for(CompteBancaire compte : compteClient.getComptesBancaire()){
+                                    if(compte instanceof CompteEpargne){
+                                        numeroCompte = compte.getNumeroCompte();
+                                        break;
+                                    }
+                                }
+
+                                if(numeroCompte != null){
+                                    cnx.setNumeroCompteSelectionne(numeroCompte);
+                                    cnx.envoyer("SELECT OK");
+                                }else{
+                                    cnx.envoyer("SELECT NO")
+                                }
+                            }else{
+                                cnx.envoyer("SELECT NO");
+                            }
+                            
+                        }else{
+                            cnx.envoyer("SELECT NO");
+                        }
+                    }
+                    break;
 
                 /******************* TRAITEMENT PAR DÉFAUT *******************/
                 default: //Renvoyer le texte recu convertit en majuscules :
