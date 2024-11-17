dd872bc9e48ee98a90bccf42ae9e4ddf2a7341c6
BrouillardLiam <liambrouillard@outlook.fr>
2024-11-17 09:14:18 -0500

    FAIT: 7.1 creation de la classe operation

---
+++ src/main/java/com/atoudeft/banque/Operation.java
@@ -0,0 +1,35 @@
+public class Operation {
+    protected double montant;
+    protected Date date;
+    protected String typeOperation;
+
+    public Operation(double montant, String typeOperation){
+        this.montant = montant;
+        this.typeOperation = typeOperation;
+        this.date = new Date();
+    }
+
+    public double getMontant() {
+        return montant;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getTypeOperation() {
+        return typeOperation;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setTypeOperation(String typeOperation) {
+        this.typeOperation = typeOperation;
+    }
+
+    public void setMontant(double montant) {
+        this.montant = montant;
+    }
+}
